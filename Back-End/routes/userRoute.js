cae84ab97f531c9741e94d28b4dc0e7ac33950e2
0KevinB <92813801+0KevinB@users.noreply.github.com>
2023-12-07 14:52:44 -0500

    Modulo GET y UPDATE

--- Back-End/routes/userRoute.js
+++ Back-End/routes/userRoute.js
@@ -41,7 +41,7 @@ router.post('/login', (req, res) => {
             } else if (results[0].Estado === 0) {
                 return res.status(401).json({ message: "Espere aprovacion del administrador" });
             } else if (results[0].Contraseña == user.Contraseña) {
-                const response = { CorreoElectronico: results[0].CorreoElectronico, Rol: results[0].Estado };
+                const response = { Cedula: results[0].Cedula, CorreoElectronico: results[0].CorreoElectronico, Rol: results[0].Estado };
                 const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
                     expiresIn: "8h",
                 });
@@ -55,6 +55,35 @@ router.post('/login', (req, res) => {
     })
 })
 
+router.get('/get', (req, res) => {
+    var query = "select CEDULA, NOMBRE, CORREOELECTRONICO from USUARIOS where rolid=1";
+    coneccion.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results)
+        } else {
+            return res.status(500).json(err)
+        }
+    })
+})
 
+router.patch('/update', (req, res) => {
+    let user = req.body;
+    var query2 = "update USUARIOS set estado=? where cedula=?";
+    coneccion.query(query2, [user.estado, user.Cedula], (err, results) => {
+
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "Error updating" });
+            }
+            return res.status(200).json({ message: "User update successfully" });
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.get('/checkToken', (req, res) => {
+    return res.status(200).json({ message: "Activo" })
+})
 
 module.exports = router;
